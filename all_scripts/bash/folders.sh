b4f14b83958e0cecd7d7aed7cd8087ed2c65e6de
Maria <mariaropat@gmail.com>
2018-02-16 23:48:04 +0100

    automatic push

--- all_scripts/bash/folders.sh
+++ all_scripts/bash/folders.sh
@@ -1,7 +1,17 @@
+mkdir $1
+cd $1
+
 mkdir all_scripts
+echo "#all_scripts" > all_scripts/README.md
+
 mkdir input_dir
+echo "#input_dir" > input_dir/README.md
+
 mkdir output_dir
+echo "#output_dir" > output_dir/README.md
+
 mkdir datasets
+echo "#datasets" > datasets/README.md
 
 cd all_scripts
 mkdir bash
