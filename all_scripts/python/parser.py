107993af7feee22a813229aa43ac9ffea1561d5c
Maria <mariaropat@gmail.com>
2018-02-22 22:20:08 +0100

    automatic push

---
+++ all_scripts/python/parser.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def parser(filename):
+    
+    sequence = []
+    structure = []
+
+    with open(filename,"r") as fh:
+        
+            
+
+if __name__ == "__main__":
+    parser("testset.txt")
+
