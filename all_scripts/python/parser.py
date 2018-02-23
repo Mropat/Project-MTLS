f2b72a99a57cb3cdabaa514e8001d1184afafb36
Maria <mariaropat@gmail.com>
2018-02-23 18:05:07 +0100

    automatic push

--- all_scripts/python/parser.py
+++ all_scripts/python/parser.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 def parser(filename):
     
@@ -14,8 +15,12 @@ def parser(filename):
     return sequence, structure
 
 
-def int_encode(sequence, structure):
-    structure_hash = {"H" : 0, "S" : 1, "C" : 2}
+def int_encode(structure):
+    
+    structure_hash = {"H" : 0,
+                      "S" : 1,
+                      "C" : 2}
+    
     int_structure = []
     for unit in structure:
         int_unit = ""
@@ -23,13 +28,18 @@ def int_encode(sequence, structure):
             char = structure_hash[char]
             int_unit += str(char)
         int_structure.append(int_unit)
-    
 
-    sequence_hash = {"A" : 0,
+    return int_structure
+
+
+def sequence_vectors(sequence, window, enc_structure):
+
+    window_toarrays = []
+    sequence_hash = {"X" : 0,
                      "R" : 1,
                      "N" : 2,
                      "D" : 3,
-                     "B" : 4,
+                     "A" : 4,
                      "C" : 5,
                      "E" : 6,
                      "Q" : 7,
@@ -48,9 +58,31 @@ def int_encode(sequence, structure):
                      "Y" : 20,
                      "V" : 21}
     
-            
+    for unit in sequence:
+        for aa in range (len(unit)-window):            
+            window_list = unit[aa : aa+window]
+            intwindow_list =[]
+            for char in window_list:
+                char = sequence_hash[char]
+                intwindow_list.append(char)
+            window_toarrays.append(intwindow_list)
+
+    yvec_l = []
+    for unit in enc_structure:
+        yvec_l.append(unit[window//2:len(unit)-window//2])
+
+    yvec = list(itertools.chain.from_iterable(yvec_l))
+
+    yvec = np.asarray(yvec)
+    window_toarrays = np.asarray(window_toarrays)
+    print (window_toarrays)
     
+
+    return window_toarrays, yvec
+
+
 
 if __name__ == "__main__":
     sequence, structure = parser("testset.txt")
-    int_encode(sequence, structure)
+    sequence_vectors (sequence, 3, int_encode(structure))
+    
