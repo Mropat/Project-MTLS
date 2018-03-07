392fdf992e99cff7cefab8fb111922449c769bdf
Maria <mariaropat@gmail.com>
2018-03-07 23:42:05 +0100

    automatic push

---
+++ all_scripts/bash/psiblast.sh
@@ -0,0 +1,4 @@
+export BLASTDB=/local_uniref/uniref/uniref90
+
+for f in *; do if [ ! -f ../PSSM_out/$f.psiblast ]; then psiblast -query $f -db uniref90.db -num_iterations 3 -evalue 0.001 -out ../PSSM_out/$f.psiblast -out_ascii_pssm ../PSSM_out/$f.pssm -num_threads 8; fi; done
+
--- all_scripts/python/LinearSVC_model_xval+ws.py
+++ all_scripts/python/LinearSVC_model_xval+ws.py
@@ -62,46 +62,14 @@ def svm_train(X, Y):
 
     score = cross_val_score(clf, X, Y)
     print (score)
-    return clf
-
-def svm_input(filename, window):
-
-    test_vector = []
-    test_vector_frames = []
-    padding = ""
-    
-    for l in range(window//2):
-        padding = padding + "0"    
-    
-    with open(filename, "r") as fh:
-        protseq = padding + fh.readline().strip() + padding
-        for c in protseq:
-            if c.isalpha():
-                test_vector.append(ord(c))
-            else:
-                test_vector.append(int(c))
-    
-    for i in range(len(test_vector)-window+1):
-        test_vector_frames.append(test_vector[i: i+window])
-    test_vector_frames = np.array(test_vector_frames)
-    test_v_enc = encoder.fit_transform(test_vector_frames)
-    
-
-    out_prediction=colonel.predict(test_v_enc)
-    result =[]
-    for char in out_prediction:
-        result.append(chr(char))
-    result=''.join(result)
-    print(protseq[window//2 : len(protseq) - window//2])
-    print(result)  
 
 
 if __name__ == "__main__":
-    window = 13
-    sequence, structure = parser("fullset.txt")
-    enc_sequence = int_encode(sequence, window)
-    enc_structure = int_encode(structure, window)
-    sequence_vec, structure_vec = sequence_vectors(enc_sequence, enc_structure, window)
-    encoder = OHE()    
-    colonel = svm_train(encoder.fit_transform(sequence_vec), structure_vec)
-    svm_input("pred", window)
+    for w in range (5, 27, 2):
+        window = w
+        sequence, structure = parser("fullset.txt")
+        enc_sequence = int_encode(sequence, window)
+        enc_structure = int_encode(structure, window)
+        sequence_vec, structure_vec = sequence_vectors(enc_sequence, enc_structure, window)
+        encoder = OHE()    
+        colonel = svm_train(encoder.fit_transform(sequence_vec), structure_vec)
