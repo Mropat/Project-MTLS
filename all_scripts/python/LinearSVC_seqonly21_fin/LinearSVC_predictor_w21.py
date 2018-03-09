424e520c3b51c9e7f6d95a1371cd2f26752c3c20
Maria <mariaropat@gmail.com>
2018-03-09 12:33:34 +0100

    automatic push

--- all_scripts/python/LinearSVC_seqonly21_fin/LinearSVC_predictor_w21.py
+++ all_scripts/python/LinearSVC_seqonly21_fin/LinearSVC_predictor_w21.py
@@ -27,39 +27,40 @@ def predict_fasta(filename, window):
             line = fh.readline()
 
 
-    for pid in prot_id:
-        for seq in sequence:
+    for pidn in range (len(prot_id)):
+        seq = sequence[pidn]
+        pid = prot_id[pidn]
+                    
+        test_vector = []
+        test_vector_frames = []
             
-            test_vector = []
-            test_vector_frames = []
-            
-            for res in seq:
-                if res.isalpha():
+        for res in seq:
+            if res.isalpha():
                     test_vector.append(ord(res))
-                else:
-                    test_vector.append(int(res))
+            else:
+                test_vector.append(int(res))
 
 
-            vectenc = pickle.load(open ("ohe.sav", "rb"))
+        vectenc = pickle.load(open ("ohe.sav", "rb"))
 
-            for i in range(len(test_vector)-window+1):
-                test_vector_frames.append(test_vector[i: i+window])
-            test_vector_frames = np.array(test_vector_frames)
-            test_v_enc = vectenc.transform(test_vector_frames)
+        for i in range(len(test_vector)-window+1):
+            test_vector_frames.append(test_vector[i: i+window])
+        test_vector_frames = np.array(test_vector_frames)
+        test_v_enc = vectenc.transform(test_vector_frames)
                         
             
 
-            linclf = pickle.load(open("LinearSVC_3SSTRIDE_w21.sav", "rb"))  
-            out_prediction=linclf.predict(test_v_enc)
-            result =[]
-            for char in out_prediction:
-                result.append(chr(char))
-            result=''.join(result)
+        linclf = pickle.load(open("LinearSVC_3SSTRIDE_w21.sav", "rb"))  
+        out_prediction=linclf.predict(test_v_enc)
+        result =[]
+        for char in out_prediction:
+            result.append(chr(char))
+        result=''.join(result)
 
-            with open ("LinSVC21_predictions", "a+") as wh:
-                wh.write(pid + "\n")
-                wh.write(seq[window//2 : len(seq) - window//2] + "\n")
-                wh.write(result + "\n"+"\n")
+        with open ("LinSVC21_predictions", "a+") as wh:
+            wh.write(pid + "\n")
+            wh.write(seq[window//2 : len(seq) - window//2] + "\n")
+            wh.write(result + "\n"+"\n")
 
 if __name__ == "__main__":
     window = 21
