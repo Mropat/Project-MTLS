9dfa82e8ff09477b1373a1d05468cec9f5070374
Maria <mariaropat@gmail.com>
2018-03-12 13:47:26 +0100

    changes

--- all_scripts/python/connectsets.py
+++ all_scripts/python/connectsets.py
@@ -19,26 +19,46 @@ def feature_vecs (protid,  window):
     paddingmtx = np.zeros((window//2, 20))
     offset = window // 2  
 
+    str_vec = []
     seq_vec = []
-    for prot in protid:
+    
+    for ind, prot in enumerate(protid):
+                
+        strc = structures[ind]
+        
+        for pos in strc:
+            str_vec.append(ord(pos))
+
         pssm = pssmdict[prot]
         pssm = np.append(pssm, paddingmtx, axis=0)
-        pssm = np.append(paddingmtx, pssm, axis=0)     
+        pssm = np.append(paddingmtx, pssm, axis=0)
+        testshape = np.array([])     
         for i in range(offset, pssm.shape[0]-offset):
             features = pssm[i-offset : i+offset+1].flatten()
             seq_vec.append(features)
+            testshape = np.concatenate([testshape, features])
+        #print (testshape.shape[0])
+        #print (len(strc)*260)
+        if testshape.shape[0] != len (strc*260):
+            print (protid [ind])
             
-    pickle.dump(seq_vec, open(dumps, "wb"))
+#    print (len(str_vec))
+#    print (np.array([seq_vec]).shape)
+        
+        
+        
+
+    
             
+    #pickle.dump(seq_vec, open(dumps, "wb"))      
     
 
     
 
-if __name__ == "__main__":
-    for window in range (13,23, 2):
-        window = window 
-        dumps = "seq_vec"+str(window)+".sav"
-        protid, structures = (get_sets("datasets/3sstride_full.txt"))  
+if __name__ == "__main__":     
+    for window in range (13,15, 2):
+        protid, structures = (get_sets("datasets/3sstride_full.txt")) 
+        dumps = "seq_vec%i.sav" % window
         feature_vecs(protid, window)
     
     print("all done")
